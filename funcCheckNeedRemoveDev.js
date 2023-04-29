37b700c24306bdba4de1f2faf678616f7e98ec98
Shredi87 <bannikov8787@gmail.com>
2023-04-29 15:05:27 +0300

    feat: add func for check need remove developers

---
+++ funcCheckNeedRemoveDev.js
@@ -0,0 +1,29 @@
+// каскад функций, проверяющий равен ли счетчик дней простоя программистов в отделе (department.countWaitDays) трем.
+// при верном условии запускает метод увольнения программиста
+// также следит за изменением счетчика дней простоя программистов в отделе (department.countWaitDays)
+export function funcCheckNeedRemoveDev(department) {
+  let waitDays = department.getCountWaitDays(); // количество дней простоя берем из отдела
+
+  if (checkListWaitingDevDepartment(department)) { // если в списке простоя есть хоть кто-то
+    if (checkQuantityWaitngDays(waitDays)) { // проверяем равен ли простой программистов трем дням
+      department.setCountWaitDays(waitDays); // передаем в счетчик простоя отдела тож значение "три"
+      teamLead.removeDev(department); // увольняем программиста
+      return; // и выходим
+    }
+
+    waitDays++; // если счетчик простоя меньше трех увеличиваем его на один
+    department.setCountWaitDays(waitDays); // возвращаем значение в счетчик простоя отдела
+    return; // и выходим из функции
+  }
+  waitDays = 0; // при пустом списке ожидания дни ожидания обнуляются
+  department.setCountWaitDays(waitDays); // и счетчик дней простоя в отделе тоже
+  return; // выходим из функции
+}
+
+function checkListWaitingDevDepartment(department) {
+  return (department.listWaitingDev.length !== 0);
+}
+
+function checkQuantityWaitngDays(waitDays) {
+  return (waitDays === 3);
+}
