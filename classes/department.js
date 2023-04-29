d1868fcd06fd6d716045f003e10393da89be5aef
Shredi87 <bannikov8787@gmail.com>
2023-04-29 15:07:19 +0300

    fix: add count wait days and his methods

--- classes/department.js
+++ classes/department.js
@@ -2,12 +2,28 @@ class Department {
   listWaitProject // очередь ожидающих проектов
   listWorkingProject // список проектов в работе
   listWaitingDev // список простаивающих программистов
+  #countWaitDays // счетчик дней простоя программистов
   typeDepartment // тип отдела
 
   constructor() {
     this.listWaitProject = [];
     this.listWorkingProject = [];
     this.listWaitingDev = [];
+    this.#countWaitDays = 0;
+  }
+
+  getCountWaitDays() {
+    return this.#countWaitDays;
+  }
+
+  setCountWaitDays(waitDays) {
+    if (waitDays === 0) {
+      this.#countWaitDays = 0;
+    } else if (waitDays === 3) {
+      this.#countWaitDays = 3;
+    } else {
+      this.#countWaitDays += 1;
+    }
   }
 
   get typeDepartment() {
