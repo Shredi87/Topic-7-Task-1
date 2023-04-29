d484cf790bcdc2902cb5fb5224b6cfa58eae5424
Shredi87 <bannikov8787@gmail.com>
2023-04-29 16:42:29 +0300

    fix:rename func

--- funcCheckNeedRemoveDev.js
+++ funcCheckNeedRemoveDev.js
@@ -1,14 +1,14 @@
 // каскад функций, проверяющий равен ли счетчик дней простоя программистов в отделе (department.countWaitDays) трем.
 // при верном условии запускает метод увольнения программиста
 // также следит за изменением счетчика дней простоя программистов в отделе (department.countWaitDays)
-export function funcCheckNeedRemoveDev(department) {
+export function checkNeedRemoveDev(department) {
   let waitDays = department.getCountWaitDays(); // количество дней простоя берем из отдела
 
   if (checkListWaitingDevDepartment(department)) { // если в списке простоя есть хоть кто-то
     if (checkQuantityWaitngDays(waitDays)) { // проверяем равен ли простой программистов трем дням
       department.setCountWaitDays(waitDays); // передаем в счетчик простоя отдела тож значение "три"
       teamLead.removeDev(department); // увольняем программиста
-      return; // и выходим
+      return; // и выходим из функции
     }
 
     waitDays++; // если счетчик простоя меньше трех увеличиваем его на один
