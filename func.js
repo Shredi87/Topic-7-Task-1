18b315c8f53d67ba1244ee499b9d0b0de4d38842
Shredi87 <bannikov8787@gmail.com>
2023-04-30 21:36:22 +0300

    fix: change code structure

--- func.js
+++ func.js
@@ -1,4 +1,49 @@
-// Функция-рандомайзер используется в соответствующих проектах для определения:
+import { teamLead } from "./classes/teamLead.js";
+import { office } from './classes/department.js';
+import { checkNeedRemoveDev } from "./funcCheckNeedRemoveDev.js";
+
+export function workingOffice(days) {
+
+  for (let i = 1; i <= days; i++) {
+    console.log(`workingOffice day ${i}`);
+    // Начало рабочего дня
+    // Проверяем наличие законченных проектов и переводим освободившихся программистов в список простоя
+    office.forEach(department => department.checkWorkSpace());
+
+    // Отправляем в работу оставшиеся со вчера проекты и освободившихся разработчиков
+    office.forEach(department => department.fillWorkSpace());
+
+    // Проверяем наличие проектов в списке ожидания и нанимаем на них программистов
+    office.forEach(teamLead.checkStack());
+    // teamLead.checkStack(webDepartment);
+    // teamLead.checkStack(mobilDepartment);
+    // teamLead.checkStack(testDepartment);
+
+    // Директор генерирует новые проекты
+    teamLead.getProjects();
+
+    // Отправляем в работу новые проекты со свободными разработчиками
+    office.forEach(department => department.fillWorkSpace());
+
+    // Сортируем списки простоя программистов по количеству проектов
+    office.forEach(department => department.sortListWaitDev());
+
+    // Проверяем необходимость увольнения программистов
+    office.forEach(department => checkNeedRemoveDev(department));
+    // checkNeedRemoveDev(webDepartment);
+    // checkNeedRemoveDev(mobilDepartment);
+    // checkNeedRemoveDev(testDepartment);
+  }
+
+  return console.log(`За ${days} дней было сформировано: 
+  ${office[0].getCountFinishedProject()} веб проектов,
+  ${office[1].getCountFinishedProject()} мобильных проектов,
+  ${office[2].getCountFinishedProject()} и выпущено в работу  проектов. 
+  А также нанято ${office[0].getCountHireDev()} и уволено ${office[0].getCountRemoveDev()} веб-разработчиков,
+          нанято ${office[1].getCountHireDev()} и уволено ${office[1].getCountRemoveDev()} мобильных разработчиков,
+          нанято ${office[2].getCountHireDev()} и уволено ${office[2].getCountRemoveDev()} QA специалистов.`);
+  
+}// Функция-рандомайзер используется в соответствующих проектах для определения:
 // 1. типов проектов (веб|мобильные) в функции chooseTypeProject
 // 2. количества проектов (от 0 до 4) в функции chooseQuantityProjects
 // 3. сложности проектов (от 1 до 3) для каждого проекта выбираются отдельно chooseDifficultProject
