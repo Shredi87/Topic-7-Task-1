35ea45e2dc0512ea12c5abcc504b1513eb8142e4
Shredi87 <bannikov8787@gmail.com>
2023-05-08 10:16:17 +0300

    fix: teamLead.js

--- classes/teamLead.js
+++ classes/teamLead.js
@@ -1,7 +1,3 @@
-import { WebProject, MobilProject } from './project.js';
-import { MobDev, WebDev, TestDev } from './developer.js';
-import { webDepartment, mobilDepartment } from "../func.js";
-
 class TeamLead {
 
   // Метод служит для проверки очереди накопившихся проектов
@@ -15,7 +11,7 @@ class TeamLead {
     } 
   }
 
-  // Метод найма программиста
+  // Метод найма программиста  --- проверен/исправен
   hireDev(typeProject) {
     let dev;
     switch (typeProject) {
@@ -33,21 +29,29 @@ class TeamLead {
     return dev;
   }
 
-  // генерация новых проектов
   getProjects() {
     let typeProject = chooseTypeProject();  // возвращает 'MOBIL' или 'WEB'
+    console.log('typeProject   ' + typeProject);
     let quantityProjects = chooseQuantityProjects(); // возвращает целое число от 0 до 4
+    console.log('quantityProjects   ' + quantityProjects);
+    console.log('_______________________________________________________________________________________________________');
     // Прогоняем цикл на создание проектов в зависимости от количества проектов. Если количество проектов равно нуля - цикл не запускается.
     for (let i = 0; i < quantityProjects; i++) {
       let difficultProject = chooseDifficultProject(); // возвращает сложность для каждого проекта от 1 до 3
+      console.log('difficultProject   ' + difficultProject);
       if (typeProject === 'WEB') { // если тип проекта веб
         let project = new WebProject(difficultProject); // создаем новый веб проект с определенной выше сложностью
+        console.log(project);
         webDepartment.listWaitProject.push(project); // и добавляем его в массив веб проектов созданных этим днем
+        webDepartment.setCountComingProject();
+        console.log(webDepartment.getCountComingProject());
       } 
       if (typeProject === 'MOBIL') { // если тип проекта мобильный
         let project = new MobilProject(difficultProject); // создаем новый мобильный проект с определенной выше сложностью
-        console.log('mob ' + project);
+        console.log(project);
         mobilDepartment.listWaitProject.push(project); // и добавляем его в массив мобильных проектов созданных этим днем
+        mobilDepartment.setCountComingProject();
+        console.log(mobilDepartment.getCountComingProject());
       }
     }
   }
@@ -58,36 +62,4 @@ class TeamLead {
     department.listWaitDev.shift();
     department.setCountRemoveDev();
   }
-}
-
-
-export let teamLead = new TeamLead();
-
-// Функция-рандомайзер используется в соответствующих проектах для определения:
-// 1. типов проектов (веб|мобильные) в функции chooseTypeProject
-// 2. количества проектов (от 0 до 4) в функции chooseQuantityProjects
-// 3. сложности проектов (от 1 до 3) для каждого проекта выбираются отдельно chooseDifficultProject
-function randomInteger(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-}
-
-// 1. Выбор типа проектов
-// Тип проектов поступивших в один день может быть только веб или только мобильные
-// где 0 - веб-проекты, а 1 - мобильные проекты.
-function chooseTypeProject() {
-  let typeProject = randomInteger(0, 1);
-  return (typeProject === 1) ? 'MOBIL' : 'WEB';
-}
-
-// 2. Определение количества проектов
-function chooseQuantityProjects() {
-  let quantityProjects = randomInteger(0, 4);
-  return quantityProjects;
-}
-
-// 3. Определение сложности каждого проекта
-function chooseDifficultProject() {
-  let difficultProject = randomInteger(1, 3);
-  return difficultProject;
 }
