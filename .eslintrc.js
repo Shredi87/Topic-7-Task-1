1e4aa7b96fae4df20f25dd270f37ae82c6ab213b
Shredi87 <bannikov8787@gmail.com>
2023-05-26 13:40:50 +0300

    fix: gitignore & eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "overrides": [
+  ],
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+  "rules": {
+    "indent": ["error", 2, { "ignoreComments": true }],
+    "quotes": ["error", "single", { "avoidEscape": true }, { "allowTemplateLiterals": true }],
+    "semi": ["error", "always", { "omitLastInOneLineBlock": true }, { "omitLastInOneLineClassBody": true }],
+    "no-empty": "error",
+    "no-cond-assign": "error",
+    "for-direction": "error",  
+  }
 }
